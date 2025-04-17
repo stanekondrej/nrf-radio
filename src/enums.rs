515f48972d67ec4d3c912cbb4d870901de30b1b8
Ondřej Staněk <staneko@duck.com>
2025-04-17 09:38:01 +0200

    moved enums to separate file

---
+++ src/enums.rs
@@ -0,0 +1,88 @@
+//! Constants defined in order to work with the radio more easily. See their respective
+//! documentation for more details.
+
+/// Radio state; useful for determining whether it's in `Tx`, `Rx`, or `Disabled` mode
+#[derive(Debug)]
+pub enum State {
+    /// The radio is disabled
+    Disabled = 0,
+    /// The radio is ramping up into `Rx`
+    RxRu = 1,
+    /// The radio is waiting to receive something
+    RxIdle = 2,
+    /// The radio is receiving something
+    Rx = 3,
+    /// The radio is being disabled while in `Rx`
+    RxDisable = 4,
+    /// The radio is ramping up to transmit something
+    TxRu = 9,
+    /// The radio is waiting to transmit something
+    TxIdle = 10,
+    /// The radio is transmitting something
+    Tx = 11,
+    /// The radio is being disabled while in `Tx`
+    TxDisable = 12,
+}
+
+/// Transmission power
+#[allow(missing_docs)] // these are self-explanatory
+pub enum TxPower {
+    Pos4dBm = 0x04,
+    _0dBm = 0x00,
+    Neg4dBm = 0xFC,
+    Neg8dBm = 0xF8,
+    Neg12dBm = 0xF4,
+    Neg16dBm = 0xF0,
+    Neg20dBm = 0xEC,
+    Neg30dBm = 0xD8,
+}
+
+/// The different protocols and speeds supported by the radio. The `Nrf` modes are
+/// Nordic's proprietary implementations, while `Ble1Mbit is just` Bluetooth low energy.
+#[allow(missing_docs)] // better described by the enum doc
+pub enum Mode {
+    Nrf1Mbit = 0,
+    Nrf2Mbit = 1,
+    Nrf250Kbit = 2,
+    Ble1Mbit = 3,
+}
+
+/// **Basically** simpler representations of in-air physical addresses.
+#[allow(missing_docs)] // better described by the enum doc
+pub enum LogicalAddress {
+    _0 = 0,
+    _1 = 1,
+    _2 = 2,
+    _3 = 3,
+    _4 = 4,
+    _5 = 5,
+    _6 = 6,
+    _7 = 7,
+}
+
+/// Events emitted by the radio when certain things happen.
+///
+/// A good example is the `END` event that is emitted **when a packet is received,**
+/// and at the end of the transmission of a packet.
+#[derive(PartialEq, Eq, Clone, Copy)]
+pub enum Event {
+    /// RADIO has ramped up and is ready to be started
+    Ready = 0,
+    /// Address sent or received
+    Address = 1,
+    /// Packet payload sent or received
+    Payload = 2,
+    /// Packet sent or received
+    End = 3,
+    /// RADIO has been disabled
+    Disabled = 4,
+    /// A device address match occurred on the last received packet
+    DevMatch = 5,
+    /// No device address match occurred on the last received packet
+    DevMiss = 6,
+    /// Sampling of receive signal strength complete. A new RSSI sample is ready for readout from the
+    /// RSSISAMPLE register.
+    RSSIEnd = 7,
+    /// Bit counter reached bit count value specified in the BCC register
+    BCMatch = 10,
+}
--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@
 #![no_std]
 #![deny(missing_docs)]
 
+pub mod enums;
 pub mod packet;
 
 use core::{marker::PhantomData, mem};
@@ -15,92 +16,6 @@ use nrf51_hal::pac;
 pub const MAX_PACKET_LENGTH: usize = 254;
 
 type PacketBuf = [u8; MAX_PACKET_LENGTH];
-
-/// Radio state; useful for determining whether it's in `Tx`, `Rx`, or `Disabled` mode
-#[derive(Debug)]
-pub enum State {
-    /// The radio is disabled
-    Disabled = 0,
-    /// The radio is ramping up into `Rx`
-    RxRu = 1,
-    /// The radio is waiting to receive something
-    RxIdle = 2,
-    /// The radio is receiving something
-    Rx = 3,
-    /// The radio is being disabled while in `Rx`
-    RxDisable = 4,
-    /// The radio is ramping up to transmit something
-    TxRu = 9,
-    /// The radio is waiting to transmit something
-    TxIdle = 10,
-    /// The radio is transmitting something
-    Tx = 11,
-    /// The radio is being disabled while in `Tx`
-    TxDisable = 12,
-}
-
-/// Transmission power
-#[allow(missing_docs)] // these are self-explanatory
-pub enum TxPower {
-    Pos4dBm = 0x04,
-    _0dBm = 0x00,
-    Neg4dBm = 0xFC,
-    Neg8dBm = 0xF8,
-    Neg12dBm = 0xF4,
-    Neg16dBm = 0xF0,
-    Neg20dBm = 0xEC,
-    Neg30dBm = 0xD8,
-}
-
-/// The different protocols and speeds supported by the radio. The `Nrf` modes are
-/// Nordic's proprietary implementations, while `Ble1Mbit is just` Bluetooth low energy.
-#[allow(missing_docs)] // better described by the enum doc
-pub enum Mode {
-    Nrf1Mbit = 0,
-    Nrf2Mbit = 1,
-    Nrf250Kbit = 2,
-    Ble1Mbit = 3,
-}
-
-/// **Basically** simpler representations of in-air physical addresses.
-#[allow(missing_docs)] // better described by the enum doc
-pub enum LogicalAddress {
-    _0 = 0,
-    _1 = 1,
-    _2 = 2,
-    _3 = 3,
-    _4 = 4,
-    _5 = 5,
-    _6 = 6,
-    _7 = 7,
-}
-
-/// Events emitted by the radio when certain things happen.
-///
-/// A good example is the `END` event that is emitted **when a packet is received,**
-/// and at the end of the transmission of a packet.
-#[derive(PartialEq, Eq, Clone, Copy)]
-pub enum Event {
-    /// RADIO has ramped up and is ready to be started
-    Ready = 0,
-    /// Address sent or received
-    Address = 1,
-    /// Packet payload sent or received
-    Payload = 2,
-    /// Packet sent or received
-    End = 3,
-    /// RADIO has been disabled
-    Disabled = 4,
-    /// A device address match occurred on the last received packet
-    DevMatch = 5,
-    /// No device address match occurred on the last received packet
-    DevMiss = 6,
-    /// Sampling of receive signal strength complete. A new RSSI sample is ready for readout from the
-    /// RSSISAMPLE register.
-    RSSIEnd = 7,
-    /// Bit counter reached bit count value specified in the BCC register
-    BCMatch = 10,
-}
 
 struct Transmitter;
 struct Receiver;
@@ -150,7 +65,7 @@ impl<M> Radio<M> {
     }
 
     /// Checks the state that the radio is currently in.
-    pub fn check_state(&self) -> State {
+    pub fn check_state(&self) -> enums::State {
         let state = self.radio.state.read().bits();
         unsafe { mem::transmute(state as u8) }
     }
@@ -167,14 +82,14 @@ impl<M> Radio<M> {
     }
 
     /// Enables interrupts to be emitted by the event specified
-    pub fn enable_event(&self, event: Event) {
+    pub fn enable_event(&self, event: enums::Event) {
         self.radio
             .intenset
             .write(|w| unsafe { w.bits(0b1 << event as u8) });
     }
 
     /// Unsets the bit signaling the firing of an event
-    pub fn clear_event(&self, event: Event) {
+    pub fn clear_event(&self, event: enums::Event) {
         self.radio
             .intenclr
             .write(|w| unsafe { w.bits(0b1 << event as u8) });
@@ -193,25 +108,25 @@ impl<M> Radio<M> {
     /// Note that when called outside of a context invoked due to an event, this function
     /// will return [`Event::Ready`], because it has the value 0x0, and therefore there
     /// is literally no way to tell that event and no event apart.
-    pub fn get_active_event(&self) -> Option<Event> {
+    pub fn get_active_event(&self) -> Option<enums::Event> {
         // TODO: surely this can be rewritten as a macro or something...
         let events = [
-            Event::Ready,
-            Event::Address,
-            Event::Payload,
-            Event::End,
-            Event::Disabled,
-            Event::DevMatch,
-            Event::DevMiss,
-            Event::RSSIEnd,
-            Event::BCMatch,
+            enums::Event::Ready,
+            enums::Event::Address,
+            enums::Event::Payload,
+            enums::Event::End,
+            enums::Event::Disabled,
+            enums::Event::DevMatch,
+            enums::Event::DevMiss,
+            enums::Event::RSSIEnd,
+            enums::Event::BCMatch,
         ];
         let reg = self.radio.intenset.read().bits();
         events.into_iter().find(|&e| reg & (0b1 << e as u32) != 0)
     }
 
     /// Sets the radio mode (protocol, tx/rx speed, etc..)
-    pub fn set_mode(&self, mode: Mode) {
+    pub fn set_mode(&self, mode: enums::Mode) {
         self.radio.mode.write(|w| unsafe { w.bits(mode as u32) });
     }
 
@@ -274,7 +189,7 @@ impl Radio<Receiver> {
     /// Enable listening on a logical address. This is different from the actual
     /// addresses that the radio uses internally, but is much simpler to use and is
     /// probably what you want.
-    pub fn enable_rx_address(&self, address: LogicalAddress) {
+    pub fn enable_rx_address(&self, address: enums::LogicalAddress) {
         //   1011_0101
         // | 0100_0000
         //   ---------
@@ -289,7 +204,7 @@ impl Radio<Receiver> {
     }
 
     /// Disables a logical address for listening. See [`Radio::enable_rx_address`].
-    pub fn disable_rx_address(&self, address: LogicalAddress) {
+    pub fn disable_rx_address(&self, address: enums::LogicalAddress) {
         //   0010_0110
         // ^ 0000_0100
         //   ---------
@@ -329,7 +244,7 @@ impl Radio<Transmitter> {
     }
 
     /// Set the logical address to send packets from.
-    pub fn set_tx_address(&self, address: LogicalAddress) {
+    pub fn set_tx_address(&self, address: enums::LogicalAddress) {
         self.radio
             .txaddress
             .write(|w| unsafe { w.txaddress().bits(address as u8) });
@@ -337,7 +252,7 @@ impl Radio<Transmitter> {
 
     /// Sets the power (in dB) with which the radio should broadcast. More power = higher
     /// energy consumption.
-    pub fn set_tx_power(&self, power: TxPower) {
+    pub fn set_tx_power(&self, power: enums::TxPower) {
         self.radio
             .txpower
             .write(|w| unsafe { w.txpower().bits(power as u8) });
