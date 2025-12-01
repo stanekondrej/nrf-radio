e8b56cb4f262d82935e4db1c9e32c9ee7d36c049
Ondřej Staněk <viego443@proton.me>
2025-12-01 16:17:31 +0100

    skeleton

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,8 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+#![no_std]
+#![no_main]
+#![deny(missing_docs)]
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+//! An abstraction over the nRFxxxx SoCs' radio peripheral
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+/// The main RADIO abstraction
+pub struct Radio {}
